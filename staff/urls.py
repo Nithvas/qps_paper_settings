c0389be58ca2c3b9c3ceea5d1d45a65a5ea4fbe4
Nithvas <nithvas18@gmail.com>
2026-05-15 16:09:24 +0530

    correction

--- staff/urls.py
+++ staff/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'staff'
 
 urlpatterns = [
-    path('', views.staff_list, name='staff_listt'),
+    path('', views.staff_list, name='staff_list'),
     path('add/<int:id>/', views.staff_add, name='staff_add'),
     path('edit/<int:id>/', views.staff_edit, name='staff_edit'),
     path('delete/<int:id>/', views.staff_delete, name='staff_delete'),
